645f3aa16e0bf1c993662433802b0054992af9e6
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2022-08-19 16:24:40 +0700

    fixing stash main.dart line 7 dan line 8

--- main.dart
+++ main.dart
@@ -4,4 +4,6 @@ void main() {
   stdout.writeln('Version Control System');
   stdout.writeln('Hello, Miki-Chan');
   stdout.writeln('Hello, Maria');
+  stdout.writeln('Ame taberudesuka?');
+  stdout.writeln('Hai tabetaidesu! :D');
 }
